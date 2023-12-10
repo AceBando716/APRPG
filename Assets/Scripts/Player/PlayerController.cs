f667344f1e6b271836c865e4d88ff108d4ec1c47
AceBando716 <apleasant@berklee.edu>
2023-12-09 23:37:28 -0500

    Animations+newModel

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,57 +1,145 @@
 using UnityEngine;
 using Cinemachine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
     private Rigidbody rb;
     private CinemachineVirtualCamera virtualCamera;
 
-    void Start()
+    private bool isSprinting;
+    private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode dashKey = KeyCode.Space;
+
+    private bool isDashing;
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashDuration = 0.5f;
+    [SerializeField] private float dashDrag = 1.5f;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        if (rb == null) Debug.LogError("Rigidbody not found on the player.");
 
-        // Get the Cinemachine Virtual Camera component
         virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        if (virtualCamera == null) Debug.LogError("CinemachineVirtualCamera not found in the scene.");
+
+        animator = GetComponent<Animator>();
+        if (animator == null) Debug.LogError("Animator not found on the player.");
+
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        // Get input for movement along the Horizontal and Vertical axes
+        HandleSprinting();
+        HandleGrounded();
+        HandleMovement();
+        HandleDashing();
+    }
+
+    private void HandleSprinting()
+    {
+        if (Input.GetKeyDown(sprintKey) && isGrounded)
+        {
+            isSprinting = true;
+        }
+        else if (Input.GetKeyUp(sprintKey) || !isGrounded)
+        {
+            isSprinting = false;
+        }
+
+        animator.SetBool("IsSprinting", isSprinting);
+    }
+
+    private void HandleGrounded()
+    {
+        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundCheckDistance, whatIsGround);
+        animator.SetBool("IsGrounded", isGrounded);
+    }
+
+    private void HandleMovement()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         Vector3 inputDirection = new Vector3(x, 0f, y).normalized;
 
-        // Check if input magnitude is significant enough for movement
-        if (inputDirection.magnitude >= 0.1f)
-        {
-            // Get the camera's forward and right directions
-            Vector3 cameraForward = virtualCamera.transform.forward;
-            Vector3 cameraRight = virtualCamera.transform.right;
+        float speed = isSprinting ? sprintSpeed : walkSpeed;
 
-            // Calculate the movement direction based on camera orientation
+        bool isMoving = inputDirection.magnitude >= 0.1f;
+        animator.SetBool("IsWalking", isMoving);
+
+        if (isMoving)
+        {
+            Vector3 cameraForward = virtualCamera.transform.forward;
+            cameraForward.y = 0;  
+            Vector3 cameraRight = virtualCamera.transform.right;
+            cameraRight.y = 0;  
+
             Vector3 moveDirection = (cameraForward * y + cameraRight * x).normalized;
 
-            // Rotate the player towards the movement direction
             float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            // Move the player based on the calculated direction and speed
-            MovePlayer(moveDirection);
+            MovePlayer(moveDirection, speed);
         }
     }
 
-    void MovePlayer(Vector3 direction)
+    private void MovePlayer(Vector3 direction, float speed)
     {
-        // Calculate the movement and apply it to the Rigidbody
         Vector3 movement = direction * speed * Time.fixedDeltaTime;
         rb.MovePosition(transform.position + movement);
     }
+
+    private void HandleDashing()
+    {
+        if (Input.GetKeyDown(dashKey) && !isDashing && isGrounded)
+        {
+            isDashing = true;
+            animator.SetBool("IsDashing", true);
+            animator.SetTrigger("DashTrigger");
+            StartCoroutine(Dash());
+        }
+        else if (!Input.GetKeyDown(dashKey) || !isGrounded)
+        {
+            animator.SetBool("IsDashing", false);
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        float startTime = Time.time;
+        Vector3 dashDirection = transform.forward;
+
+        rb.velocity = dashDirection * dashSpeed;
+
+        while (Time.time < startTime + dashDuration)
+        {
+            rb.AddForce(-rb.velocity.normalized * dashDrag, ForceMode.Acceleration);
+            yield return null;
+        }
+
+        rb.velocity = Vector3.zero;
+        isDashing = false;
+    }
 }
+
+
+
+
+
+
 
 
 
